304a648be1a9f4643408487e855db5050e19792b
Ahmed Mohamed Abd El Ftah <ahmedabdelftah95165@gmail.com>
2026-03-11 09:55:35 +0200

    Add single post and all posts

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/all-posts', function () {
+    return view('all-posts');
+});
+
+Route::get('/single-post', function () {
+    return view('single-post');
+});
