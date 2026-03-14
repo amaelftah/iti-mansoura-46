4189cb19a18006ff6331fe51ac2374e416e2b61b
Ahmed Mohamed Abd El Ftah <ahmedabdelftah95165@gmail.com>
2026-03-14 10:25:36 +0200

    Add laravel breeze and Customized middleware

---
+++ app/Http/Middleware/Customized.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Exception;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class Customized
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        throw new Exception('you are not allowed');
+        
+
+        return $next($request);
+    }
+}
