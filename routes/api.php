d1982d2661d584b1761d0778e458197a2b4f47f7
Ahmed Mohamed Abd El Ftah <ahmedabdelftah95165@gmail.com>
2026-03-15 10:34:58 +0200

    Add laravel sanctum

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,37 @@
 use App\Http\Controllers\API\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 
-Route::get('posts', [PostController::class, 'index']);
+Route::get('posts', [PostController::class, 'index'])->middleware('auth:sanctum');
 Route::get('posts/{post}', [PostController::class, 'show']);
 Route::post('posts', [PostController::class, 'store']);
+
+#1- issue api token
+Route::post('/sanctum/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required',
+    ]);
+ 
+    $user = User::where('email', $request->email)->first();
+ 
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+ 
+    return $user->createToken($request->device_name)->plainTextToken;
+});
+
+
+#2- validate api token
